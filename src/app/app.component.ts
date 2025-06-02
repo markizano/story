7ef189e6078da3656752581f5d0eedb0bec30a83
Markizano Draconus <markizano@markizano.net>
2025-06-02 11:55:32 -0500

    Getting angular to behave for the home page. Setting up base components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'story-web';
+  title = "The Adventures of Markizano Draconus!";
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,67 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from 'app/pages/home/home.component';
+import { SignupComponent } from 'app/pages/signup/signup.component';
+import { LoginComponent } from 'app/pages/login/login.component';
+import { LogoutComponent } from 'app/pages/logout/logout.component';
+import { CharactersComponent } from 'app/pages/characters/characters.component';
+import { CharacterDetailComponent } from 'app/pages/characters/character-detail.component';
+import { StoriesComponent } from 'app/pages/stories/stories.component';
+import { StoryDetailComponent } from 'app/pages/stories/story-detail.component';
 
-export const routes: Routes = [];
+/*
+/: Home page, intro to story, CTA to sign up.
+/signup: Sign up page, form to create a new account.
+/login: Login page, form to log in to an existing account.
+/logout: Logout page, action to log out of the current session.
+/characters: List of characters, view details of each character.
+/characters/:id: View details of a specific character.
+/stories: List of stories, view details of each story.
+/stories/:id: View details of a specific story.
+*/
+export const routes: Routes = [
+    {
+        component: HomeComponent,
+        title: 'Home',
+        path: '',
+        children: [],
+    }, {
+        component: SignupComponent,
+        title: 'Sign Up',
+        path: 'signup',
+    }, {
+        component: LoginComponent,
+        title: 'Login',
+        path: 'login',
+    }, {
+        component: LogoutComponent,
+        title: 'Logout',
+        path: 'logout',
+    }, {
+        path: 'characters',
+        title: 'Characters',
+        children: [
+            {
+                path: '',
+                component: CharactersComponent,
+            }, {
+                path: ':id',
+                component: CharacterDetailComponent,
+            }
+        ]
+    }, {
+        path: 'story',
+        title: 'Stories',
+        children: [
+            {
+                path: '',
+                component: StoriesComponent,
+            }, {
+                path: ':id',
+                component: StoryDetailComponent,
+            }
+        ]
+    }, {
+        path: '*',
+        redirectTo: '/'
+    }
+];
