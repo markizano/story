eee19e56fc281e337f04ed8bb9de402e09bdc2c9
Markizano Draconus <markizano@markizano.net>
2025-06-11 08:20:40 -0500

    adding new forgot password component.

---
+++ src/app/pages/login/forgotpw.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mz-forgotpw',
+  imports: [],
+  templateUrl: './forgotpw.component.html',
+  styleUrl: './forgotpw.component.css'
+})
+export class ForgotpwComponent {
+
+}
