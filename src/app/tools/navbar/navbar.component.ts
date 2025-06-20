8998627cd22c8a10669b337ddcec8b033fc82a95
Markizano Draconus <markizano@markizano.net>
2025-06-20 09:38:16 -0500

    adding new component for navigation bar once authenticated.

---
+++ src/app/tools/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mz-navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+}
